83ae952d766935afe239fbc0b4784594933deefa
Jaroslaw Romanczuk <szamotulok@gmail.com>
2025-02-01 01:23:18 +0100

    Deploy to GitHub Page2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: NotFoundComponent }
+];
 
-export const routes: Routes = [];
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
